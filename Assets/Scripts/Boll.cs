44de02a5a6cd615190f49c558ad2c82c2a421fea
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-11-26 15:46:26 +0100

    ADDERING SKIT

--- Assets/Scripts/Boll.cs
+++ Assets/Scripts/Boll.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Boll : MonoBehaviour
 {
     [SerializeField]
@@ -19,28 +19,35 @@ public class Boll : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(new Vector3(0, 0, 500));
+            rb.AddForce(new Vector3(0, 0, 250));
             rb.velocity = Vector3.zero;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(new Vector3(0, 0, -500));
+            rb.AddForce(new Vector3(0, 0, -250));
             rb.velocity = Vector3.zero;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(new Vector3(500, 0, 0));
+            rb.AddForce(new Vector3(250, 0, 0));
             rb.velocity = Vector3.zero;
 
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(new Vector3(-5000, 0, 0));
+            rb.AddForce(new Vector3(-250, 0, 0));
             rb.velocity = Vector3.zero;
 
 
         }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Death")
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
     }
 }
