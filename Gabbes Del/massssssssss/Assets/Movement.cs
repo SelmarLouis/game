b52c719eeafd14838cad76ae2154eb9fcae383c8
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-11-26 10:15:12 +0100

    Gabbes Del added

---
+++ Gabbes Del/massssssssss/Assets/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector3(0, 0, 250));
+         rb.velocity = Vector3.zero;
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(new Vector3(0, 0, -250));
+            rb.velocity = Vector3.zero;
+        }
+         if(Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(new Vector3(-250, 0, 0));
+            rb.velocity = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(new Vector3(250, 0, 0));
+            rb.velocity = Vector3.zero;
+        }
+       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Death")
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+        }
+    }
+
+}
