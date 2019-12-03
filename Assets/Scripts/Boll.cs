6d2d878565c148e0bd37e1a682af258f26c307ea
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-12-03 11:23:19 +0100

    AI ändringar

--- Assets/Scripts/Boll.cs
+++ Assets/Scripts/Boll.cs
@@ -19,21 +19,24 @@ public class Boll : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(new Vector3(0, 0, 25));
+            rb.velocity = new Vector3(rb.velocity.x, 0, 5);
+            //rb.AddForce(new Vector3(0, 0, 25));
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(new Vector3(0, 0, -25));
+            rb.velocity = new Vector3(rb.velocity.x, 0, -5);
+            //rb.AddForce(new Vector3(0, 0, -25));
         }
-
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(new Vector3(25, 0, 0));
+            rb.velocity = new Vector3(5, 0, rb.velocity.z);
+            //rb.AddForce(new Vector3(25, 0, 0));
 
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(new Vector3(-25, 0, 0));
+            rb.velocity = new Vector3(-5, 0, rb.velocity.z);
+            //rb.AddForce(new Vector3(-250, 0, 0));
         }
 
 
