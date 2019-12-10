8ae0f6117efcced78bb11ced3ceb1202c0729709
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-12-10 17:32:38 +0100

    add

---
+++ Assets/Scripts/AI_Pathfinder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AI_Pathfinder : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
