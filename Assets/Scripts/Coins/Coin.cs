f316ee9c415a5f9be24a5b61b47d9729e9598e15
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-12-16 20:48:23 +0100

    script

--- Assets/Scripts/Coins/Coin.cs
+++ Assets/Scripts/Coins/Coin.cs
@@ -8,14 +8,6 @@ public class Coin : MonoBehaviour
     [SerializeField]
     Text coinCounter;
     [SerializeField]
-    public float elapsedTime = 0.0f;
-    [SerializeField]
-    public float pointTimer = 20.0f;
-    [SerializeField]
-    public float pointCounter = 0;
-    [SerializeField]
-    public float pointAdd = 5;
-    [SerializeField]
     Text highScoreText;
 
     public static int points;
@@ -27,13 +19,8 @@ public class Coin : MonoBehaviour
 
     private void Update()
     {
-        elapsedTime += Time.deltaTime;
-        if (elapsedTime > pointTimer)
-        {
-            pointCounter += pointAdd;
-            elapsedTime = 0;
-        }
-            coinCounter.text = "Coins: " + points.ToString();
+       
+            coinCounter.text = "Coins collected: " + points.ToString();
 
     }
 }
---
+++ Assets/Scripts/Coins/Pointcounter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pointcounter : MonoBehaviour
+{
+    [SerializeField]
+    Text pointCounter;
+    [SerializeField]
+    public float elapsedTime = 0.0f;
+    [SerializeField]
+    public float pointTimer = 20.0f;
+    [SerializeField]
+    public float pointCounter1 = 0;
+    [SerializeField]
+    public float pointAdd = 5;
+    [SerializeField]
+    public float pointSum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > pointTimer)
+        {
+
+            elapsedTime = 0;
+        }
+        pointCounter.text = "Coins earned:" + pointSum.ToString();
+    }
+}
