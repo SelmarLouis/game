8a0ac28d9c3502c584a8c21d1306ae4beb9010c4
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-12-16 16:50:10 +0100

    Komenterat min kod som jag skrivit (gabbe)

--- Assets/Scripts/POV.cs
+++ Assets/Scripts/POV.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class POV : MonoBehaviour
 {
     [SerializeField]
-    Transform player;
-    Vector3 offset;
+    Transform player; //Sätter positionen på kameran på "spelaren".
+    Vector3 offset; //säger till vilket avstånd det ska vara med en 3D vector.
     void Start()
     {
-        offset = transform.position - player.position;
+        offset = transform.position - player.position; //position på cameran
     }
 
 
     void Update()
     {
-        transform.position = player.position + offset;
+        transform.position = player.position + offset; //flyttar kameran genom hela tiden som det körs.
     }
 }
