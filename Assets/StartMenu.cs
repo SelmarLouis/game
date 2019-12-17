98ac5a324cfda846ddcc50d4123c896192e6adac
SelmarLouis <roeselerselmar@gmail.com>
2019-12-17 10:51:43 +0100

    Selmars kommentarer på koden

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-
+    /*(SELMAR) Play-knappens funktion är "Startgame()". Denna säger att när knappen trycks ned så spelas nästa scen i build index upp som då är 
+    spelscenen. Den säger också att Exit-knappens funktion är att utföra metoden "Exitgame()" som innebär att när Exit-knappen trycks ned så "stängs spelet ned"
+    vilket inte går att visa i Unity så spelet visar att det stängs ned genom att skicka EXIT GAME i consolen istället.*/
     public void Startgame()
     
     {
