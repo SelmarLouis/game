498c30d28f347b96b6b70ba970e83feb65c35411
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2019-12-17 11:05:44 +0100

    migu

--- Assets/Scripts/AI/AI_Pathfinder.cs
+++ Assets/Scripts/AI/AI_Pathfinder.cs
@@ -7,26 +7,20 @@ public class AI_Pathfinder : MonoBehaviour
 {
    
     [SerializeField]
-    GameObject target;
+    GameObject target; //(Gabriel) Säger till att gameobject heter target.
 
-    NavMeshAgent agent;
+    NavMeshAgent agent;//(Gabriel) säger till att man ska använda unitys navmeshagent.
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>(); //(Gabriel) Hämtar Navmeshagent
     }
 
 
     void Update()
     {
-        agent.SetDestination(target.transform.position);
+        agent.SetDestination(target.transform.position);//(Gabriel) bestämmer position den ska till
     }
-
-    /*(SELMAR) När spelaren kolliderar med agent:ens sphere collider kollar agent:en om det är ett objekt med 
-    tag:en "Player" som har kolliderat med den. Ifall det var en spelare så förföljer agent:en spelaren genom Update metoden ovan. 
-    Så fort spelaren lämnar agent:ens Sphere collider radius blir agentens target till sig själv igen och den slutar följa seplaren.
-    Denna kod gör att agent:en inte jagar spelaren i all oändlighet utan bara om spelaren kolliderar med agent:ens sphere collider.
-    Det ska funka ungefär som en search-radius för motståndaren.*/
 
     private void OnTriggerEnter(Collider other)
     {
